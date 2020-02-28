9521e0d4c777bb9f9f48209b3571c0f7e311136f
Lee-Roy King <leeroywking@gmail.com>
2020-02-27 19:47:16 -0800

    mwah

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require('body-parser')
+const cors = require('cors');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const userCheck = require('./userGet.js');
